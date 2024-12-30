56add585a7b3c04f0f9abb0b0ae604ee1760c374
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-30 20:44:55 +0200

    First commit
    Empty Repository

---
+++ SwiftUIPrep/SwiftUIPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPrepApp.swift
+//  SwiftUIPrep
+//
+//  Created by Vlad on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
